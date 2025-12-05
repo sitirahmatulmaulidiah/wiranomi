cc23a447a31136a250011237b958164c22003f9b
sitirahmatulmaulidiah <rahmatulsiti21@gmail.com>
2025-12-05 13:29:11 +0800

    Merapikan game drag & drop

--- wiranomi_project/urls.py
+++ wiranomi_project/urls.py
@@ -1,9 +1,15 @@
 # wiranomi_project/urls.py
 
 from django.contrib import admin
-from django.urls import path, include # Tambahkan 'include'
+from django.urls import path, include
+from django.conf import settings             # <-- Tambahan 1
+from django.conf.urls.static import static   # <-- Tambahan 2
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('pembelajaran.urls')), # <-- Tambahkan baris ini
+    path('', include('pembelajaran.urls')),
 ]
+
+# --- Tambahkan Blok Ini di Paling Bawah ---
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
